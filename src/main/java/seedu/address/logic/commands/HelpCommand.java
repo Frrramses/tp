365c662d0bc2379cbb5f7a5972ec69572faf203b
Frrramses <56105845+Frrramses@users.noreply.github.com>
2022-10-15 10:41:09 +0800

    First Draft of Help - Frances
    A basic directory on how to use address book.

--- src/main/java/seedu/address/logic/commands/HelpCommand.java
+++ src/main/java/seedu/address/logic/commands/HelpCommand.java
@@ -9,10 +9,16 @@ public class HelpCommand extends Command {
 
     public static final String COMMAND_WORD = "help";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Shows program usage instructions.\n"
-            + "Example: " + COMMAND_WORD;
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Shows program usage instructions.\n";
 
-    public static final String SHOWING_HELP_MESSAGE = "Opened help window.";
+    public static final String SHOWING_HELP_MESSAGE = "Opened help window.\n"
+        + "add: "+ "Add a contact.\n"
+        + "delete: "+ "Delete a contact.\n" 
+        + "edit "+ "Edit a contact.\n" 
+        + "list "+ "List all contacts.\n" 
+        + "find "+ "Find a person by name.\n" 
+        + "exit "+ "Exit addressbook.\n"   ;
+
 
     @Override
     public CommandResult execute(Model model) {
